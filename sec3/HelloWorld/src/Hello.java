927ccacf426e1bbfff10262e56c7480128f7a846
Rambod Rahmani <rambodrahmani@yahoo.it>
2017-08-15 01:20:35 +0200

    Section 3 source codes added.

---
+++ sec3/HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * Created by Rambod Rahmani.
+ * Section: Section 3 - First Steps
+ * Project: HelloWorld
+ *
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
